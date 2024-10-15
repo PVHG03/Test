071bdf44045725b59648e5d18aab3054d83d5fe1
PVHG03 <HungPV.B21VT222@stu.ptit.edu.vn>
2024-10-15 13:17:15 +0700

    Initialized Client and Server project folder

---
+++ Server/src/main/java/com/ltm/server/Server.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ltm.server;
+
+/**
+ *
+ * @author phamh
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
